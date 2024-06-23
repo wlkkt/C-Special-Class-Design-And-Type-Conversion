936e320843c21a58dd8838e85b656d8ceb760716
wlkkt <yangyf0618@qq.com>
2024-06-23 22:59:31 +0800

    完善

--- C++特殊类设计及类型转换/test.cpp
+++ C++特殊类设计及类型转换/test.cpp
@@ -99,13 +99,108 @@ namespace hunger
 	Singleton Singleton::_sint(1, 1, { "陕西","四川" });
 }
 
+
+namespace lazy
+{
+	class Singleton
+	{
+	public:
+		static Singleton* GetInstance()
+		{
+			// 第一次调用时，创建单例对象，第二次时_psint就不为空，进入该函数也只是返回_psint
+			//存在线程安全问题，需要加锁
+			if (_psint == nullptr)
+			{
+				_psint = new Singleton;//可能需要释放
+			}
+
+			return _psint;
+		}
+
+		static void DelInstance()
+		{
+			//释放 + 置空
+			if (_psint)
+			{
+				delete _psint;
+				_psint = nullptr;
+			}
+		}
+
+		void Print()
+		{
+			cout << _x << endl;
+			cout << _y << endl;
+
+			for (auto& e : _vstr)
+			{
+				cout << e << " ";
+			}
+			cout << endl;
+		}
+
+		void AddStr(const string& s)
+		{
+			_vstr.push_back(s);
+		}
+
+		Singleton(Singleton const&) = delete;
+		Singleton& operator=(Singleton const&) = delete;
+
+	private:
+		Singleton(int x = 0, int y = 0, const vector<string>& vstr = { "yyyyy","xxxx" })
+			:_x(x)
+			, _y(y)
+			, _vstr(vstr)
+		{}
+
+		~Singleton()
+		{
+			// 把数据写到文件
+			cout << "~Singleton()" << endl;
+		}
+
+
+		int _x;
+		int _y;
+		vector<string> _vstr;
+		static Singleton* _psint;
+
+		// 内部类，用于防止用户忘记显示调用DelInstance释放对象
+		//实例化一个静态内部类成员对象（全局生命周期），当程序结束时就会调用GC类对象的析构进而调用DelInstance
+		class GC
+		{
+		public:
+			~GC()
+			{
+				Singleton::DelInstance();
+			}
+		};
+		static GC gc;//不实例化GC类对象，该类没用不会调用该类的析构函数
+	};
+
+	// 两个静态成员对象在类外的定义
+	Singleton* Singleton::_psint = nullptr;
+	Singleton::GC Singleton::gc;//什么都不做
+}
+
+
 int main()
 {
-	hunger::Singleton::GetInstance()->Print();
-	hunger::Singleton::GetInstance()->AddStr("甘肃");
-	hunger::Singleton::GetInstance()->Print();
-	hunger::Singleton::GetInstance()->AddStr("甘肃");
-	hunger::Singleton::GetInstance()->Print();
+	//hunger::Singleton::GetInstance()->Print();
+	//hunger::Singleton::GetInstance()->AddStr("甘肃");
+	//hunger::Singleton::GetInstance()->Print();
+	//hunger::Singleton::GetInstance()->AddStr("甘肃");
+	//hunger::Singleton::GetInstance()->Print();
+
+
+	lazy::Singleton::GetInstance()->Print();
+	lazy::Singleton::GetInstance()->AddStr("甘肃");
+	lazy::Singleton::GetInstance()->Print();
+	lazy::Singleton::GetInstance()->AddStr("甘肃");
+	lazy::Singleton::GetInstance()->Print();
+
+	//lazy::Singleton::DelInstance();//显示调用DelInstance可以释放，不显示也可以（注释和非注释两次运行试一试即可）
 	return 0;
 }
 
